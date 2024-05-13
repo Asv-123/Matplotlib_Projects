994214c92d29d1f114f8b9681b379ed60f30a30e
Asv-123 <169059737+Asv-123@users.noreply.github.com>
2024-05-13 18:06:55 +0400

    Add files via upload
    In this project you can analyze around 50 cites and their rainfall with the help of matplotlib. I have also included the CSV file which I used but you can just substitue it for your own if you wish to.

---
+++ Rainfall_Analysis.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+data = pd.read_csv('/content/raindata.csv')
+print(data)
+
+plt.plot(data.columns[2:],data.iloc[1,2:], marker='o',label = 'New york')
+plt.plot(data.columns[2:],data.iloc[5,2:], marker='o',label = 'los Angles')
+plt.plot(data.columns[2:],data.iloc[10,2:], marker='o',label = 'Chicago')
+plt.plot(data.columns[2:],data.iloc[15,2:], marker='o',label = 'Houston')
+plt.plot(data.columns[2:],data.iloc[20,2:], marker='o',label = 'Pheonix')
+
+
+plt.grid()
+plt.xlabel('Months')
+plt.ylabel('Rainfall in mm')
+plt.title('Rainfall in different cities in 2015')
+plt.legend()
+plt.show()
+
+
+
+
+
